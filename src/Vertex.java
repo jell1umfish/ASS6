ed4c383bd5bf48219fc031082dcedd6eaa44b2ca
jell1umfish <jell1umfish@gmail.com>
2023-06-01 01:23:48 +0600

    vertex

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices; // with weights
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(data);
+    }
+}
