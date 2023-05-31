0b44ee753a340debdbf0c4f9283d60b2a6649f3a
jell1umfish <jell1umfish@gmail.com>
2023-06-01 01:18:42 +0600

    addvertex addedge

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -6,4 +6,16 @@ public class WeightedGraph<V extends Comparable<V>> {
     public WeightedGraph() {
         map = new HashMap<>();
     }
+
+    public void addVertex(Vertex<V> vertex) {
+        map.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
+        if (!map.containsKey(source) || !map.containsKey(destination)) {
+            throw new IllegalArgumentException("Vertex is not found in the graph");
+        }
+        source.addAdjacentVertex(destination, weight);
+        map.get(source).add(destination);
+    }
 }
